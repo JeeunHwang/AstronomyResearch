3bf5c2b37951b1022e398aa05e9d95b72bbb3199
jeeun hwang <jehwang@qso.snu.ac.kr>
2022-02-06 16:36:49 +0900

    new file

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  6 16:32:41 2022
+
+@author: jehwang
+"""
+
+# git test
+
+import pandas as pd
+from astropy.io import ascii
+from astropy.coordinates import SkyCoord
+
+a = 3
+b = 4
